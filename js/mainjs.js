616f06bc24886cdcc8563a1cf5813228d6285bec
chandramouli-git <60184634+chandramouli-git@users.noreply.github.com>
2021-07-03 12:48:22 +0530

    Update mainjs.js

--- js/mainjs.js
+++ js/mainjs.js
@@ -7,15 +7,15 @@ console.log(urlParams.get("uname"));
 $(document).ready(function(){
     $("#bbb1").click(function(){
         if(urlParams.get('x')==1)
-               location.replace("http://localhost:3000/donate.html?x="+urlParams.get('x'));
+               location.replace("./donate.html?x="+urlParams.get('x'));
         if(urlParams.get('x')==2)
-               location.replace("http://localhost:3000/donate.html?x="+urlParams.get('x'));
+               location.replace("./donate.html?x="+urlParams.get('x'));
     });
     $("#bbb2").click(function(){
        if(urlParams.get('x')==1)
-       location.replace("http://localhost:3000/check/:"+urlParams.get('x'));
+       location.replace("./check/:"+urlParams.get('x'));
 if(urlParams.get('x')==2)
-       location.replace("http://localhost:3000/check/:"+urlParams.get('x'));
+       location.replace("./check/:"+urlParams.get('x'));
     })
   });
 
